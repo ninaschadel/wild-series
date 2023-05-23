769425e0e82e8700c22e1690083945659960c7d4
Nina <nina.schadel@gmail.com>
2023-05-23 15:26:46 +0200

    test

--- src/DataFixtures/CategoryFixtures.php
+++ src/DataFixtures/CategoryFixtures.php
@@ -5,7 +5,7 @@ namespace App\DataFixtures;
 use App\Entity\Category;
 use Doctrine\Bundle\FixturesBundle\Fixture;
 use Doctrine\Persistence\ObjectManager;
-
+//commentaire
 class CategoryFixtures extends Fixture
 {
     public const CATEGORIES = [
