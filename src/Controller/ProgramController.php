a51022366b825b812936e32b772605cc6e7b1803
Nina <nina.schadel@gmail.com>
2023-05-24 14:50:58 +0200

    quete symfony 10

--- src/Controller/ProgramController.php
+++ src/Controller/ProgramController.php
@@ -2,6 +2,7 @@
 // src/Controller/ProgramController.php
 namespace App\Controller;
 
+use App\Repository\ProgramRepository;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
@@ -9,20 +10,21 @@ use Symfony\Component\Routing\Annotation\Route;
 class ProgramController extends AbstractController
 {
     #[Route('/program/', name: 'program_index')]
-    public function index(): Response
+    public function index(ProgramRepository $programRepository): Response
     {
+        $programs = $programRepository->findAll();
         return $this->render('program/index.html.twig', [
-            'website' => 'Wild Series',
+            'programs' => $programs,
          ]);
     }
-    #[Route('/program/{id}', methods: ['GET'], requirements: ['id' => '\d+'], name: 'app_program_show')]
-    public function show(int $id): Response
+    #[Route('/program/{id}', name: 'program_show', requirements: ['id' => '\d+'], methods: ['GET'])]
+    public function show(int $id, ProgramRepository $programRepository): Response
     {
+        $program = $programRepository->find($id);
         return $this->render(
             'program/show.html.twig',
             [
-                'controller_name' => 'ProgramController',
-                'program_id' => $id
+                'program' => $program,
             ]
         );
     }
